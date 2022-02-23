1db0d9c52f4bae00c527704a6121865a15b73206
Rizo11 <20455251@fhda.edu>
2022-02-23 21:32:39 +0500

    mud blazor added

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+using MudBlazor.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddMudServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
