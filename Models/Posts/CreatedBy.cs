cde7569bf8c8b442d87b5af22137aaa40d51be81
diyorbek013 <darkravin13@gmail.com>
2022-03-04 17:41:52 +0500

    Home Card

---
+++ Models/Posts/CreatedBy.cs
@@ -0,0 +1,8 @@
+namespace oj.fe.Models.Posts;
+
+public class CreatedBy
+{
+     public int Id { get; set; }
+     public string Username { get; set; }
+     public object Real_name { get; set; }
+}
---
+++ Models/Posts/Data.cs
@@ -0,0 +1,7 @@
+namespace oj.fe.Models.Posts;
+
+public class Data
+{
+        public List<Result> Results { get; set; }
+        public int Total { get; set; }
+}
